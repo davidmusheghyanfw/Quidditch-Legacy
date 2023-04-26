b93417b4bc67deff426e09de176e74d470376a19
davidmusheghyanfw <d.musheghyan.fw@gmail.com>
2023-04-26 20:07:42 +0400

    add visual to enemy

--- Assets/Scripts/Environment.cs
+++ Assets/Scripts/Environment.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class Environment : MonoBehaviour
 {
     [SerializeField] private GameObject burnParticle;
+    [SerializeField] private MeshRenderer mesh;
+    [SerializeField] private Color32 normalColor;
+    [SerializeField] private Color32 burnColor;
+
+    private void Start()
+    {
+        mesh.material.color = normalColor;
+    }
 
     public void IsDamaged(bool value)
     {
         burnParticle.SetActive(value);
+        mesh.material.color = value ? burnColor : normalColor;
     }
 }
